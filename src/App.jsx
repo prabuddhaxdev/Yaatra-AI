8d3e9894f1525dfeaff0d4fb8ddc55a8258fd967
prabuddha610 <prabuddha610@gmail.com>
2025-06-23 22:16:13 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <>
+      <h2>AI Trip Planner</h2>
+      <Button>Subscribe</Button>
+    </>
+  )
+}
+
+export default App
