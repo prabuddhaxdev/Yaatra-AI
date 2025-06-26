3ad18f8c065833bc8189c26a0856d021ee6b739e
prabuddha610 <prabuddha610@gmail.com>
2025-06-27 01:07:57 +0530

    Add react autocomplete google

--- src/components/custom/Hero.jsx
+++ src/components/custom/Hero.jsx
@@ -1,16 +1,24 @@
 import React from 'react'
 import { Button } from '../ui/button';
+import { Link } from 'react-router';
 
 function Hero() {
   return (
     <div className="flex flex-col items-center mx-56 gap-9">
       <h1 className="font-extrabold text-[50px] text-center mt-16">
-        <span className='text-[#f56551]'>Discover Your Next Adventure with AI: </span>Personalized
-        Itineraries
+        <span className="text-[#f56551]">
+          Discover Your Next Adventure with AI:{" "}
+        </span>
+        Personalized Itineraries
       </h1>
-      <p className='text-xl text-gray-500 text-center'>Your personal trip planner and travel curator, creating custom itineraries tailored to your interests and budget.</p>
+      <p className="text-xl text-gray-500 text-center">
+        Your personal trip planner and travel curator, creating custom
+        itineraries tailored to your interests and budget.
+      </p>
 
-      <Button>Get Started, It's Free</Button>
+      <Link to={'/create-trip'}>
+        <Button>Get Started, It's Free</Button>
+      </Link>
     </div>
   );
 }
--- src/create-trip/index.jsx
+++ src/create-trip/index.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
+import ReactGoogleAutocomplete from 'react-google-autocomplete'
 
 function CreateTrip() {
+  const [place,setPlace] = useState();
   return (
-    <div>
-      CreateTrip 
+    <div className="sm:px-10 md:px-32 lg:px-56 xl:px-10 px-5 mt-10">
+      <h2 className="font-bold text-3xl">Tell us your travel preferences.</h2>
+      <p className="mt-3 text-gray-500 text-xl">
+        Just provide us your basic information, and our trip planner will
+        generate a customized itinerary based on your preferences
+      </p>
+
+      <div className="mt-20">
+        <div>
+          <h2 className="text-xl my-3 font-medium">
+            What is the destination of your choice
+          </h2>
+          <ReactGoogleAutocomplete
+            apiKey={import.meta.env.VITE_GOOGLE_PLACE_API_KEY}
+            selectProps={{place, onChange:(v)=>{setPlace(v)}}}
+          />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default CreateTrip
