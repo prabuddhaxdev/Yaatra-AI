aea0fe39bc10ac219173f91ea9c03b6b1872eff5
prabuddha610 <prabuddha610@gmail.com>
2025-06-26 23:39:08 +0530

    Add Hero Section

--- src/components/custom/Header.jsx
+++ src/components/custom/Header.jsx
@@ -4,7 +4,7 @@ import { Button } from '../ui/button'
 function Header() {
   return (
     <div className='p-3 shadow-sm flex justify-between items-center px-5'>
-      <img src='/public/logo.svg'/>
+      <img src='/logo.svg'/>
       <div>
         <Button>Sign In</Button>
       </div>
